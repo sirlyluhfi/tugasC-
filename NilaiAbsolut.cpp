6201b3103e6c0687be08ed1feed697d7b8dc85a0
sirlyluthfi <32449911+sirlyluhfi@users.noreply.github.com>
2017-11-07 04:46:13 +0700

    menghitung nilai absoulut (X dan Y)

---
+++ NilaiAbsolut.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,y,z;
+	
+	cin >> x;
+ 	cin >> y;
+ 	
+   if(x > y){
+    z= x - y ;
+    cout << z ;
+    
+   }
+   
+   else if (y>x){
+   	z= x-y;
+   	cout << z;
+   	
+   }
+}
