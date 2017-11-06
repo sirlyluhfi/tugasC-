625fd7c47766a5b738f20522c39393491d7131a7
sirlyluthfi <32449911+sirlyluhfi@users.noreply.github.com>
2017-11-07 04:54:51 +0700

    Menginput sebuah nilai integer  dan menentukan apakah itu Positif, Negativ atau Nol

---
+++ InputNilaiInteger.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int z;
+	
+	cout << "masukan bilangan : " ; cin >> z ;
+	
+		if ( z == 0 )
+		cout << " bilangan nol " ;
+		else if ( z <= 0)
+		cout << "bilangan negative" ;
+		else if ( z >=0)
+		cout << "bilangan positif" ;
+		}
